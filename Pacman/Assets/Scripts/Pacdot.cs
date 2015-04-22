99559b140a4ef14ff9ad05214c6c8e0a48ba491f
SuperGelito <bonikun@gmail.com>
2015-04-23 00:23:29 +0200

    Added movement and one ghost

---
+++ Pacman/Assets/Scripts/Pacdot.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D co) {
+		// Do Stuff...
+		if (co.name == "pacman")
+			Destroy(gameObject);
+		//increase points
+	}
+}
